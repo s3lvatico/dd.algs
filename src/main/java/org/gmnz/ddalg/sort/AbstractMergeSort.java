b7309b17e664e7c2a0f6332f6218240a23e9527c
Simone Monotti <s3lvatico@gmail.com>
2019-01-30 18:26:15 +0100

    manca da testare il quicksort

--- src/main/java/org/gmnz/ddalg/sort/AbstractMergeSort.java
+++ src/main/java/org/gmnz/ddalg/sort/AbstractMergeSort.java
@@ -1,13 +1,12 @@
 package org.gmnz.ddalg.sort;
 
-
-abstract class AbstractMergeSort<KEY extends Comparable<KEY>> extends AbstractSortEngine {
+abstract class AbstractMergeSort<KEY extends Comparable<KEY>> extends AbstractSortEngine<KEY> {
 
 	private KEY[] aux;
 
 
 
-	protected AbstractMergeSort(Comparable[] arr) {
+	protected AbstractMergeSort(KEY[] arr) {
 		super(arr);
 		aux = (KEY[]) new Comparable[arrayLength];
 	}
@@ -16,8 +15,10 @@ abstract class AbstractMergeSort<KEY extends Comparable<KEY>> extends AbstractSo
 
 	/**
 	 * Unisce i due subarray contenuti nell'array principale nell'intervallo
-	 * [lo..mid] e [mid+1..hi]
-	 * 
+	 * [lo..mid] e [mid+1..hi].
+	 * <p>
+	 * Precondizione: lo <= mid <= hi
+	 *
 	 * @param a
 	 * @param lo
 	 * @param mid
@@ -28,32 +29,35 @@ abstract class AbstractMergeSort<KEY extends Comparable<KEY>> extends AbstractSo
 		int i = lo;
 		int j = mid + 1;
 
-		// preparo l'array ausiliario come copia dell'originale nell'intervallo chiuso
-		// [lo..hi]
+		// copio la porzione [lo..hi] di interesse nell'array ausiliario
 		for (int k = lo; k <= hi; k++) {
-			aux[k] = (KEY) a[k];
+			aux[k] = a[k];
 		}
 
-		// fusione
+		// fusione (con riordino)
 
 		// scansione di tutto l'intervallo di interesse
+		/*
+		 * per ogni valore dell'indice k si considerano in sequenza i valori dei due
+		 * array dei quali si vuol fare la fusione . Il k-esimo posto è occupato dal
+		 * minore dei due valori indicati dagli indici di scansione i e j, fatti salvi i
+		 * casi in cui i due subarray siano "esauriti". Quando si prende uno dei valori
+		 * dei due array, il rispettivo indice di scansione viene fatto avanzare.
+		 */
 		for (int k = lo; k <= hi; k++) {
 			// ho finito a sinistra?
 			if (i > mid) {
 				// allora prendo da destra
-				a[k] = aux[j++];
-			}
-			else // ho finito a destra?
+				a[k] = aux[j++]; // e l'indice del subarray destro scorre
+			} else // ho finito a destra?
 				if (j > hi) {
 					// alllora prendo da sinistra
-					a[k] = aux[i++];
-				}
-				else // confronto i due valori nei due subarray
+					a[k] = aux[i++]; // e l'indice del subarray sinistro scorre
+				} else // confronto i due valori nei due subarray
 						 // prendo il valore più piccolo puntato dagli indici
 					if (less(aux[i], aux[j])) {
 						a[k] = aux[i++];
-					}
-					else {
+					} else {
 						a[k] = aux[j++];
 					}
 		}
--- src/main/java/org/gmnz/ddalg/sort/MergeSortTopDown.java
+++ src/main/java/org/gmnz/ddalg/sort/MergeSortTopDown.java
@@ -4,9 +4,9 @@ package org.gmnz.ddalg.sort;
 import edu.princeton.cs.algs4.In;
 
 
-public class MergeSortTopDown extends AbstractMergeSort {
+public class MergeSortTopDown<KEY extends Comparable<KEY>> extends AbstractMergeSort<KEY> {
 
-	protected MergeSortTopDown(Comparable[] arr) {
+	protected MergeSortTopDown(KEY[] arr) {
 		super(arr);
 	}
 
@@ -19,6 +19,19 @@ public class MergeSortTopDown extends AbstractMergeSort {
 
 
 
+	/**
+	 * Ordinamento vero e proprio.
+	 * <p>
+	 * Se gli indici lo e hi coincidono o si incrociano, si ferma la ricorsione.
+	 * Altrimenti viene individuato un indice "di mezzo" e viene richiamato questo
+	 * stesso metodo con i due subarray tra <code>[lo..mid]</code> e
+	 * <code>[mid+1..hi]</code>. Quando gli array non sono ulteriormente
+	 * suddivisibili si esegue il <code>merge</code> delle due parti individuate,
+	 * che ricreerà un subarray ordinato.
+	 *
+	 * @param lo
+	 * @param hi
+	 */
 	private void sort(int lo, int hi) {
 		if (hi <= lo) {
 			return;
