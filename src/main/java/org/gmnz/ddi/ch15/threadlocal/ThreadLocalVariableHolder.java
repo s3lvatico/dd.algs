a301108151a9dfe6ce255797ae8f6345ab5794b4
Simone Monotti <s3lvatico@gmail.com>
2019-02-28 18:32:29 +0100

    esperimento con threadlocal

--- src/main/java/org/gmnz/ddi/ch15/threadlocal/ThreadLocalVariableHolder.java
+++ src/main/java/org/gmnz/ddi/ch15/threadlocal/ThreadLocalVariableHolder.java
@@ -1,17 +1,38 @@
 package org.gmnz.ddi.ch15.threadlocal;
 
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
 public class ThreadLocalVariableHolder {
 
-	public static String get() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//@formatter:off
+	private static ThreadLocal<Integer> value = new ThreadLocal<Integer>() {
+
+		private Random rnd = new Random(47);
+
+		@Override
+		protected synchronized Integer initialValue() {
+			return rnd.nextInt(10000);
+		}
+	};
+
+	public static Integer get() { return value.get(); }
+	public static void increment() { value.set(value.get() + 1); }
+//@formatter:on
 
 
 
-	public static void increment() {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService pool = Executors.newCachedThreadPool();
+		for (int i = 0; i < 5; i++) {
+			pool.execute(new Accessor(i));
+		}
+		TimeUnit.SECONDS.sleep(1);
+		pool.shutdownNow();
 	}
 
 }
