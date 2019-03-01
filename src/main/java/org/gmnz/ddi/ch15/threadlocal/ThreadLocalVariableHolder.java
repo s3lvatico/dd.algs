fb3ad0a33595280b8193c8be767793bf4a6b33d8
s3lvatico <s3lvatico@gmail.com>
2019-03-01 20:20:11 +0100

    quesito sul threadlocal, da investigare di più

--- src/main/java/org/gmnz/ddi/ch15/threadlocal/ThreadLocalVariableHolder.java
+++ src/main/java/org/gmnz/ddi/ch15/threadlocal/ThreadLocalVariableHolder.java
@@ -1,31 +1,39 @@
 package org.gmnz.ddi.ch15.threadlocal;
 
 
-import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
+/**
+ * L'obiettivo è dimostrare che thread distinti che accedono al medesimo
+ * oggetto, dotato però di un campo ThreadLocal, vedono ciascuno una copia
+ * specifica di quel campo. Ad un'istanza di questa classe verrà fatto accedere
+ * un numero di thread distinti che opereranno direttamente su un campo statico.
+ * <p>
+ * Perché il campo è dichiarato static? Forse per rafforzare ancora di più il
+ * fatto che usando un campo ThreadLocal anche nelle condizioni di "massimo
+ * pericolo" si ottiene comunque la thread-safety?
+ *
+ * @author gemini
+ *
+ */
 public class ThreadLocalVariableHolder {
 
 //@formatter:off
-	private static ThreadLocal<Integer> value = new ThreadLocal<Integer>() {
-
-		private Random rnd = new Random(47);
-
-		@Override
-		protected synchronized Integer initialValue() {
-			return rnd.nextInt(10000);
-		}
-	};
-
+	private static ThreadLocalValue value = new ThreadLocalValue();
 	public static Integer get() { return value.get(); }
 	public static void increment() { value.set(value.get() + 1); }
 //@formatter:on
 
 
 
+	/**
+	 *
+	 * @param args
+	 * @throws InterruptedException
+	 */
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService pool = Executors.newCachedThreadPool();
 		for (int i = 0; i < 5; i++) {
