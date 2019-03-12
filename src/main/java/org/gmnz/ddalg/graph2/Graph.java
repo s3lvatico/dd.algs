5c013dee09dfbd49e872c5c8dda615dfa3edf653
s3lvatico <s3lvatico@gmail.com>
2019-03-12 07:40:56 +0100

    costruzione grafo ok

--- src/main/java/org/gmnz/ddalg/graph2/Graph.java
+++ src/main/java/org/gmnz/ddalg/graph2/Graph.java
@@ -18,41 +18,49 @@ class Graph {
 	}
 
 
+
 	void add(int x) {
 		if (!adj.containsKey(x)) {
 			adj.put(x, new HashSet<Integer>());
 		}
 	}
 
-	/**
-	 * occhio che questa è da rivedere
-	 * @param nodes
-	 */
+
+
 	void addSubgraph(int[] nodes) {
-		for (int i =0; i< nodes.length; i++) {
-			for (int j=i+1; j<nodes.length; j++) {
-				add(nodes[i]);
-				add(nodes[j]);
-				connect(nodes[i], nodes[j]);
-			}
+		if (nodes == null || nodes.length == 0) {
+			return;
+		}
+		int n = nodes[0];
+		add(n);
+		for (int i = 1; i < nodes.length; i++) {
+			add(nodes[i]);
+			connect(n, nodes[i]);
 		}
 	}
+
+
 
 	void connect(int x, int y) {
 		adj.get(x).add(y);
 		adj.get(y).add(x);
 	}
 
-	private Set<Integer> adjacencies(int x) {
+
+
+	Set<Integer> adjacencies(int x) {
 		return adj.get(x);
 	}
 
+
+
 	public static void main(String[] args) {
 		Graph g = new Graph();
-		g.addSubgraph(new int[] {0, 1, 2, 5, 6});
-		g.addSubgraph(new int[] {3, 5, 4});
-		g.addSubgraph(new int[] {4, 6});
-		g.addSubgraph(new int[] {7,8});
-		g.addSubgraph(new int[] {9, 10, 11, 12});
+		g.addSubgraph(new int[] { 0, 1, 2, 5, 6 });
+		g.addSubgraph(new int[] { 3, 5, 4 });
+		g.addSubgraph(new int[] { 4, 3, 5, 6 });
+		g.addSubgraph(new int[] { 7, 8 });
+		g.addSubgraph(new int[] { 9, 10, 11, 12 });
+		g.addSubgraph(new int[] { 11, 12 });
 	}
 }
