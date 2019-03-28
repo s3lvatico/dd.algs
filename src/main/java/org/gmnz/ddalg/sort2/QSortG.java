21c8becbc1d9513184744c6879b951e94d5eaafe
s3lvatico <s3lvatico@gmail.com>
2019-03-28 08:40:48 +0100

    commentato qs

--- src/main/java/org/gmnz/ddalg/sort2/QSortG.java
+++ src/main/java/org/gmnz/ddalg/sort2/QSortG.java
@@ -1,6 +1,9 @@
 package org.gmnz.ddalg.sort2;
 
 
+import java.util.Random;
+
+
 public class QSortG {
 
 	int[] sort(int[] v) {
@@ -11,9 +14,15 @@ public class QSortG {
 
 
 
+/*
+ * ricorda sempre che è nei metodi ricorsivi che devi mettere la condizione di
+ * terminazione
+ */
+
+
 	private void quicksort(int[] v, int lo, int hi) {
-		// ricorda sempre che è nei metodi ricorsivi che devi mettere la condizione di
-		// terminazione!
+		// se gli indici coincidono o sono incrociati sto cercando di ordinare un array
+		// degenere
 		if (lo >= hi) {
 			return;
 		}
@@ -34,18 +43,46 @@ public class QSortG {
 
 
 
+	/**
+	 * il cuore del qs.
+	 * <p>
+	 *
+	 *
+	 * @param v
+	 * @param lo
+	 * @param hi
+	 * @return
+	 */
 	int partition(int[] v, int lo, int hi) {
+		// determina l'indice dell'elemento rispetto al quale partizionare (lo si chiama
+		// pivot)
 		int pivotIndex = (lo + hi) / 2;
+		// seleziona (isola) il pivot
 		int pivot = v[pivotIndex];
+		// Internamente a questo ciclo while gli indici lo e hi scorrono. Il ciclo viene
+		// eseguito finché hi resta strettamente maggiore di lo
 		while (lo <= hi) {
+			// sessione di scorrimento degli indici, alla fine della quale occorre
+			// controllare ancora che gli indici rispettino la condizione all'inizio del
+			// ciclo - questo è necessario per capire se si deve fare uno scambio oppure no
 			while (pivot > v[lo]) {
+				// incrementa prima il cursore basso
+				// incrementa finché gli elementi a sinistra del pivot sono strettamente minori
+				// del pivot
 				++lo;
 			}
 			while (pivot < v[hi]) {
-				hi--;
+				// decrementa il cursore alto finché gli elementi a destra del pivot sono
+				// maggiori del pivot
+				--hi;
 			}
+			// se i cursori rispettano ancora la condizione di non decrescenza si può fare
+			// lo scambio posizionale
 			if (lo <= hi) {
 				swap(v, lo, hi);
+				// i cursori vengono comunque fatti scorrere, poiché gli elementi che sono stati
+				// scambiati di posto ora occupano sicuramente la posizione corretta rispetto al
+				// pivot
 				lo++;
 				hi--;
 			}
@@ -71,5 +108,14 @@ public class QSortG {
 		QSortG qs = new QSortG();
 		qs.sort(v);
 		print(v);
+		System.out.println();
+		Random rnd = new Random();
+		v = new int[100];
+		for (int i = 0; i < 100; i++) {
+			v[i] = rnd.nextInt(500);
+		}
+		qs.sort(v);
+		print(v);
+
 	}
 }
