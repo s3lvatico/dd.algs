40741796c940c3aaaceb1468edee3e338fb66047
s3lvatico <s3lvatico@gmail.com>
2019-04-19 08:02:08 +0200

    ho capito vagamente le proprietà dell'xor

--- src/main/java/org/gmnz/hr/WhatIsXor.java
+++ src/main/java/org/gmnz/hr/WhatIsXor.java
@@ -8,10 +8,15 @@ public class WhatIsXor {
 		int x = 3;
 		int y = 2;
 
-		int z = x ^ y;
+		int z = x ^ y; //
 		System.out.println(z);
 		int t = 5;
-		int k = t ^ x;
-		System.out.println(t);
+		int k = t ^ x; // 101 ^ 011 = 110
+		System.out.println(k);
+
+		x = 2848;
+		y = 3574;
+		z = x ^ y;
+		System.out.format("%d ^ %d = %d%n", x, y, z);
 	}
 }
