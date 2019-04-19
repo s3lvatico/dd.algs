b215a801ee3281f33519c2ea05aed943088164c1
s3lvatico <s3lvatico@gmail.com>
2019-04-19 07:53:29 +0200

    baby don't hurt me

---
+++ src/main/java/org/gmnz/hr/WhatIsXor.java
@@ -0,0 +1,17 @@
+package org.gmnz.hr;
+
+
+public class WhatIsXor {
+
+	public static void main(
+			String[] args) {
+		int x = 3;
+		int y = 2;
+
+		int z = x ^ y;
+		System.out.println(z);
+		int t = 5;
+		int k = t ^ x;
+		System.out.println(t);
+	}
+}
