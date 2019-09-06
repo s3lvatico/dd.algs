4de6f1689e1df81ed11360ce9ec91da3c9717032
Simone <s3lvatico@gmail.com>
2019-09-06 12:35:45 +0200

    unordered array max pq (da testare)

---
+++ src/test/java/org/gmnz/ddalg/pq/UnorderedArrayMaxPQTest.java
@@ -0,0 +1,15 @@
+package org.gmnz.ddalg.pq;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * UnorderedArrayMaxPQTest
+ */
+public class UnorderedArrayMaxPQTest {
+
+    @Test public void dummy() {
+        Assert.assertTrue(true);
+    }
+    
+}
