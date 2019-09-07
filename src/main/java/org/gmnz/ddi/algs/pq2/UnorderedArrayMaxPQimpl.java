9fc5f1ef25aeed175cb137fad6f83e4431598e05
S3lvatico <s3lvatico@gmail.com>
2019-09-07 17:46:44 +0200

    prima impl

---
+++ src/main/java/org/gmnz/ddi/algs/pq2/UnorderedArrayMaxPQimpl.java
@@ -0,0 +1,38 @@
+package org.gmnz.ddi.algs.pq2;
+
+/**
+ * UnorderedArrayMaxPQimpl
+ */
+public class UnorderedArrayMaxPQimpl<K extends Comparable<K>> implements PriorityQueue<K> {
+
+    @Override
+    public void insert(K key) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public K max() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public K delMax() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
