4ae55bc6fe96e4a8a15aa67f074d62c111cb699b
S3lvatico <s3lvatico@gmail.com>
2019-09-08 12:48:29 +0200

    test pq con array ordinato

---
+++ src/main/java/org/gmnz/ddi/algs/pq2/OrderedArrayMaxPQimpl.java
@@ -0,0 +1,59 @@
+package org.gmnz.ddi.algs.pq2;
+
+/**
+ * UnorderedArrayMaxPQimpl
+ */
+public class OrderedArrayMaxPQimpl<K extends Comparable<K>> extends AbstractPQ<K> implements PriorityQueue<K> {
+
+    private int n;
+
+    public OrderedArrayMaxPQimpl(int size) {
+        pq = (K[]) new Comparable[size];
+        n = 0;
+    }
+
+    private boolean less(K x, K y) {
+        return x.compareTo(y) < 0;
+    }
+
+    @Override
+    public void insert(K key) {
+
+        if (n == pq.length)
+            throw new UnsupportedOperationException("queue full");
+        int i = n - 1;
+        while (i >= 0 && less(key, pq[i])) {
+            pq[i + 1] = pq[i];
+            i--;
+        }
+        pq[i + 1] = key;
+        n++;
+    }
+
+    @Override
+    public K max() {
+        if (isEmpty())
+            return null;
+        return pq[n - 1];
+    }
+
+    @Override
+    public K delMax() {
+        if (isEmpty())
+            return null;
+        K maxKey = pq[n - 1];
+        n--;
+        return maxKey;
+    }
+
+    @Override
+    public int size() {
+        return n;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+}
--- src/test/java/org/gmnz/ddi/algs/pq2/TestOrderedArrayMaxPQ.java
+++ src/test/java/org/gmnz/ddi/algs/pq2/TestOrderedArrayMaxPQ.java
@@ -3,46 +3,47 @@ package org.gmnz.ddi.algs.pq2;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestUnorderedArrayMaxPQ {
+public class TestOrderedArrayMaxPQ {
 
     @Test
     public void queueSize() {
-        PriorityQueue<Integer> pq = PqFactory.newUnorderedArrayIntegerMaxPQ(0);
+        PriorityQueue<Integer> pq = new OrderedArrayMaxPQimpl<>(0);
         Assert.assertTrue(pq.isEmpty());
         Assert.assertEquals(0, pq.size());
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void testFailingInsert() {
-        PriorityQueue<Integer> pq = PqFactory.newUnorderedArrayIntegerMaxPQ(0);
+        PriorityQueue<Integer> pq = new OrderedArrayMaxPQimpl<>(0);
         pq.insert(42);
     }
 
     @Test
     public void testMaxWithEmptyQ() {
-        PriorityQueue<Integer> pq = PqFactory.newUnorderedArrayIntegerMaxPQ(0);
+        PriorityQueue<Integer> pq = new OrderedArrayMaxPQimpl<>(0);
         Assert.assertNull(pq.delMax());
         Assert.assertNull(pq.max());
     }
 
     @Test
     public void testNotEmptyQ() {
-        PriorityQueue<String> pq = new UnorderedArrayMaxPQimpl<>(3);
+        PriorityQueue<String> pq = new OrderedArrayMaxPQimpl<>(4);
         pq.insert("b");
+        pq.insert("d");
         pq.insert("c");
         pq.insert("a");
 
-        Assert.assertEquals(3, pq.size());
+        Assert.assertEquals(4, pq.size());
         Assert.assertFalse(pq.isEmpty());
 
-        Assert.assertEquals("c", pq.max());
-        Assert.assertEquals("c", pq.delMax());
-        Assert.assertEquals(2, pq.size());
+        Assert.assertEquals("d", pq.max());
+        Assert.assertEquals("d", pq.delMax());
+        Assert.assertEquals(3, pq.size());
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void testOverflow() {
-        PriorityQueue<String> pq = new UnorderedArrayMaxPQimpl<>(2);
+        PriorityQueue<String> pq = new OrderedArrayMaxPQimpl<>(2);
         pq.insert("b");
         pq.insert("c");
         pq.insert("a");
