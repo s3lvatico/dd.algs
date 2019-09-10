43c697c4b1b52dde026c7b33277f7d194d5a4055
Simone <s3lvatico@gmail.com>
2019-09-10 17:18:07 +0200

    fare heapsort, ma riguarda sink e swim

---
+++ src/main/java/org/gmnz/ddi/algs/sort2/HeapSort.java
@@ -0,0 +1,24 @@
+package org.gmnz.ddi.algs.sort2;
+
+
+import org.gmnz.ddi.algs.pq2.MaxHeapPQ;
+
+
+/**
+ * HeapSort
+ */
+public class HeapSort<K extends Comparable<K>> extends MaxHeapPQ<K> {
+
+    public HeapSort(K[] v) {
+        super(v.length);
+    }
+
+
+
+    public void sort() {
+        // fare
+    }
+
+
+}
+
