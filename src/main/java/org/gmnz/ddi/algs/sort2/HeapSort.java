e32b501ec3fd8499e31b551417d37bbd07cae79f
Simone <s3lvatico@gmail.com>
2019-09-11 15:11:44 +0200

    livello heapify

--- src/main/java/org/gmnz/ddi/algs/sort2/HeapSort.java
+++ src/main/java/org/gmnz/ddi/algs/sort2/HeapSort.java
@@ -1,24 +1,46 @@
 package org.gmnz.ddi.algs.sort2;
 
 
-import org.gmnz.ddi.algs.pq2.MaxHeapPQ;
-
-
 /**
  * HeapSort
  */
-public class HeapSort<K extends Comparable<K>> extends MaxHeapPQ<K> {
+public class HeapSort<K extends Comparable<K>> {
 
+    private HeapUtils<K> hu;
+    private K[] v;
+
+    @SuppressWarnings("unchecked")
     public HeapSort(K[] v) {
-        super(v.length);
+        this.v = (K[]) new Comparable[v.length + 1];
+        int i = 1;
+        for (K k : v) {
+            this.v[i] = k;
+            i++;
+        }
+        hu = new HeapUtils<>();
     }
 
 
 
     public void sort() {
-        // fare
+        heapify();
     }
 
+
+
+    void heapify() {
+        int heapSize = v.length;
+        for (int i = v.length / 2; i >= 1; i--) {
+            hu.sink(v, i, heapSize);
+        }
+    }
+
+    /*
+     * l'heapsort si compone di due passaggi: 1) trasforma l'array in un maxheap
+     * 2.1) scambia di posto la cima dell'heap con l'ultimo elemento 2.2) sink(1)
+     * 2.3) restringi la dimensione dell'heap 2.4) ripeti finché la dimensione
+     * dell'heap è >= 2
+     */
 
 }
 
---
+++ src/main/java/org/gmnz/ddi/algs/sort2/HeapUtils.java
@@ -0,0 +1,76 @@
+package org.gmnz.ddi.algs.sort2;
+
+
+/**
+ * HeapUtils - funzioni generiche su un heap
+ * <p>
+ * questa classe mi serve perché deve contenere funzionalità generiche da
+ * applicare ai vari casi d'utilizzo di un heap (finora ho visto le code con
+ * priorità e l'heapsort)
+ */
+public class HeapUtils<K extends Comparable<K>> {
+
+    /**
+     * scambia gli elementi di posto i e j nell'array v
+     * 
+     * @param v
+     * @param i
+     * @param j
+     */
+    public void swap(K[] v, int i, int j) {
+        K tmp = v[i];
+        v[i] = v[j];
+        v[j] = tmp;
+    }
+
+
+
+    /**
+     * verifica che nell'array v l'elemento di posto i sia strettamente minore
+     * dell'elemento di posto j
+     * 
+     * @param  i
+     * @param  j
+     * @return
+     */
+    public boolean less(K[] v, int i, int j) {
+        return v[i].compareTo(v[j]) < 0;
+    }
+
+
+
+    /**
+     * fa emergere l'elemento i-esimo dell'array v, rispettando l'invariante del
+     * maxHeap
+     */
+    public void swim(K[] v, int i) {
+        while (i >= 1 && less(v, i / 2, i)) {
+            swap(v, i / 2, i);
+            i /= 2;
+        }
+    }
+
+
+
+    /**
+     * fa affondare l'elemento i-esimo nell'array v, rispettando l'invariante del
+     * maxHeap
+     * 
+     * @param v
+     * @param i
+     * @param heapSize
+     */
+    public void sink(K[] v, int i, int heapSize) {
+        while (2 * i <= heapSize) {
+            int k = 2 * i;
+            if (k < heapSize && less(v, k, k + 1))
+                k++;
+            if (less(v, i, k)) {
+                swap(v, i, k);
+                i = k;
+            }
+            else
+                break;
+        }
+    }
+}
