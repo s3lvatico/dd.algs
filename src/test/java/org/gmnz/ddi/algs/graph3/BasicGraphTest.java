263a18d48f2325ee156d6646c9e68cf72bfca5fc
Simone <s3lvatico@gmail.com>
2019-09-25 16:52:05 +0200

    test sui gradi di un vertice

--- src/test/java/org/gmnz/ddi/algs/graph3/BasicGraphTest.java
+++ src/test/java/org/gmnz/ddi/algs/graph3/BasicGraphTest.java
@@ -49,4 +49,24 @@ public class BasicGraphTest {
             System.out.println(g);
         }
     }
+
+    @Test public void vertexDegree() {
+        Graph g;
+        try (Scanner s = new Scanner(BasicGraphTest.class.getResourceAsStream("/tinyGraph.txt"))) {
+            g = new BasicGraph(s);
+            Assert.assertEquals(4, g.degree(0));
+            Assert.assertEquals(1, g.degree(1));
+            Assert.assertEquals(1, g.degree(2));
+            Assert.assertEquals(2, g.degree(3));
+            Assert.assertEquals(3, g.degree(4));
+            Assert.assertEquals(3, g.degree(5));
+            Assert.assertEquals(2, g.degree(6));
+            Assert.assertEquals(1, g.degree(7));
+            Assert.assertEquals(1, g.degree(8));
+            Assert.assertEquals(3, g.degree(9));
+            Assert.assertEquals(1, g.degree(10));
+            Assert.assertEquals(2, g.degree(11));
+            Assert.assertEquals(2, g.degree(12));
+        }
+    }
 }
