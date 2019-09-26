49461fc95df2d19e01950396a3f55f9482850c6e
Simone <s3lvatico@gmail.com>
2019-09-26 12:36:33 +0200

    aggiunto minimo di documentazione, anche grafica

--- src/main/java/org/gmnz/ddi/algs/graph3/package-info.java
+++ src/main/java/org/gmnz/ddi/algs/graph3/package-info.java
@@ -1,5 +1,7 @@
 /**
- * terza revisione degli ADT e degli algoritmi dedicati ai grafi
+ * Terza revisione degli ADT e degli algoritmi dedicati ai grafi. Vedere
+ * {@code readme.md} nel package
+ * 
  */
 package org.gmnz.ddi.algs.graph3;
 
---
+++ src/main/resources/org/gmnz/ddi/algs/graph3/readme.md
@@ -0,0 +1,7 @@
+#  org.gmnz.ddi.algs.graph3
+
+## Grafo di esempio usato nei test
+![grafo_di_esempio](./sample_graph.png)
+
+## Struttura
+![struttura_classi](./package_class_diagram.png)
