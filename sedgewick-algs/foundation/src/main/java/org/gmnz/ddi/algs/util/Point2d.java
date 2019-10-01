c919af3bfbcf482462905e7dc0180fadeaec24be
S3lvatico <s3lvatico@gmail.com>
2019-10-01 08:24:36 +0200

    hashcode e equals() su Point2d

--- sedgewick-algs/foundation/src/main/java/org/gmnz/ddi/algs/util/Point2d.java
+++ sedgewick-algs/foundation/src/main/java/org/gmnz/ddi/algs/util/Point2d.java
@@ -59,4 +59,36 @@ public class Point2d implements Comparable<Point2d> {
       return "Point2d [a=" + a + ", b=" + b + "]";
    }
 
+
+
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      long temp;
+      temp = Double.doubleToLongBits(a);
+      result = prime * result + (int) (temp ^ (temp >>> 32));
+      temp = Double.doubleToLongBits(b);
+      result = prime * result + (int) (temp ^ (temp >>> 32));
+      return result;
+   }
+
+
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      Point2d other = (Point2d) obj;
+      if (Double.doubleToLongBits(a) != Double.doubleToLongBits(other.a))
+         return false;
+      if (Double.doubleToLongBits(b) != Double.doubleToLongBits(other.b))
+         return false;
+      return true;
+   }
+
 }
