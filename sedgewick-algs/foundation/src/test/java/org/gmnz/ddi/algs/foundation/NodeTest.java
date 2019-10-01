c1d2d093fe85122d17f76a5c8000c7bf5e30bd0f
Simone <s3lvatico@gmail.com>
2019-10-01 10:18:15 +0200

    test nodo con i costruttori

--- sedgewick-algs/foundation/src/test/java/org/gmnz/ddi/algs/foundation/NodeTest.java
+++ sedgewick-algs/foundation/src/test/java/org/gmnz/ddi/algs/foundation/NodeTest.java
@@ -16,6 +16,23 @@ public class NodeTest {
 
         Assert.assertNotNull(stringNode.value);
         Assert.assertNotNull(stringNode.next);
+
     }
 
+
+
+    @Test
+    public void constructors() {
+        Node<Integer> n0 = new Node<>();
+        Assert.assertNull(n0.value);
+        Assert.assertNull(n0.next);
+
+        Node<Integer> n1 = new Node<>(64);
+        Assert.assertEquals(64, n1.value.intValue());
+        Assert.assertNull(n1.next);
+
+        Node<Integer> n2 = new Node<>(42, n1);
+        Assert.assertEquals(42, n2.value.intValue());
+        Assert.assertNotNull(n2.next);
+    }
 }
