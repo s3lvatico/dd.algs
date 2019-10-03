c4d9caa122717bb816dd01b58efdd4eab400f3b2
Simone <s3lvatico@gmail.com>
2019-10-03 14:17:44 +0200

    aggiunto input e testcase da HR

--- hackerrank/src/main/java/org/gmnz/hr/FraudulentActivityNotification.java
+++ hackerrank/src/main/java/org/gmnz/hr/FraudulentActivityNotification.java
@@ -1,8 +1,11 @@
 package org.gmnz.hr;
 
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Random;
+import java.util.Scanner;
 
 
 public class FraudulentActivityNotification {
@@ -207,17 +210,49 @@ public class FraudulentActivityNotification {
 
 
 
-    public static void main(String[] args) {
-        Random r = new Random();
-        final int L = 15;
-        int[] v = new int[L];
-        for (int i = 0; i < L; i++) {
-            v[i] = r.nextInt(50);
+    // public static void main(String[] args) {
+        // Random r = new Random();
+        // final int L = 15;
+        // int[] v = new int[L];
+        // for (int i = 0; i < L; i++) {
+        //     v[i] = r.nextInt(50);
+        // }
+        // print(v);
+
+        // int d = 5;
+        // System.out.println(activityNotifications(v, d));
+
+
+    // }
+    
+    private static final Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] expenditure = new int[n];
+
+        String[] expenditureItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int expenditureItem = Integer.parseInt(expenditureItems[i]);
+            expenditure[i] = expenditureItem;
         }
-        print(v);
 
-        int d = 5;
-        System.out.println(activityNotifications(v, d));
+        int result = activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
-
 }
