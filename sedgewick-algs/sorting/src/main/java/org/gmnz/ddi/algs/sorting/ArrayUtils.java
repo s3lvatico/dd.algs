c5048884e210f3f0a012bd62c4fe8a9537f29837
Simone <s3lvatico@gmail.com>
2019-10-04 11:58:03 +0200

    ArrayUtils reso pubblico

--- sedgewick-algs/sorting/src/main/java/org/gmnz/ddi/algs/sorting/ArrayUtils.java
+++ sedgewick-algs/sorting/src/main/java/org/gmnz/ddi/algs/sorting/ArrayUtils.java
@@ -4,11 +4,14 @@ package org.gmnz.ddi.algs.sorting;
 /**
  * ArrayUtils
  */
-class ArrayUtils {
+public class ArrayUtils {
 
-    private ArrayUtils() {}
+    private ArrayUtils() {
+    }
 
-    static String toString(byte[] v) {
+
+
+    public static String toString(byte[] v) {
         StringBuilder sb = new StringBuilder("[");
         for (byte b : v) {
             sb.append(String.format("%6d", b));
@@ -17,4 +20,27 @@ class ArrayUtils {
         return sb.toString();
     }
 
+
+
+    public static String toString(int[] v) {
+        double dblHighestMagnitude = 0;
+        for (int x : v) {
+            double dblMag = Math.log10(x);
+            if (dblMag > dblHighestMagnitude)
+                dblHighestMagnitude = dblMag;
+        }
+        dblHighestMagnitude = Math.ceil(dblHighestMagnitude);
+        int spacing = Double.valueOf(Math.round(dblHighestMagnitude / 1.618)).intValue();
+        int magnitude = Double.valueOf(dblHighestMagnitude).intValue();
+
+        int formatWidth = magnitude + spacing;
+        String formatString = "%" + formatWidth + "d";
+
+        StringBuilder sb = new StringBuilder("[");
+        for (int x : v) {
+            sb.append(String.format(formatString, x));
+        }
+        sb.append(" ]");
+        return sb.toString();
+    }
 }
