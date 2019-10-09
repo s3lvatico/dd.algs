d584eda55908319d6d262c07200cda75eba44ed0
Simone <s3lvatico@gmail.com>
2019-10-09 11:37:37 +0200

    isolati i test dall'esercizio

--- hackerrank/src/test/java/org/gmnz/hr/FraudulentActivityNotificationTest.java
+++ hackerrank/src/test/java/org/gmnz/hr/FraudulentActivityNotificationTest.java
@@ -2,142 +2,56 @@ package org.gmnz.hr;
 
 
 import java.io.IOException;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.concurrent.ArrayBlockingQueue;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 
 public class FraudulentActivityNotificationTest {
 
-    static int activityNotifications(int[] expenditure, int d) {
-
-        int notifications = 0;
-
-        Queue<Integer> q = new ArrayBlockingQueue<>(d);
-
-        int maxExpense = -1;
-        for (int x : expenditure) {
-            if (x > maxExpense)
-                maxExpense = x;
-        }
-
-        for (int i = 0; i < d; i++) {
-            q.add(expenditure[i]);
-        }
-
-        int[] counts = new int[maxExpense + 1];
-
-        for (int x : q) {
-            counts[x]++;
-        }
-
-        int k = d;
-
-        boolean notificationIssued = checkForFraud(counts, d, maxExpense, expenditure[k]);
-        notifications += notificationIssued ? 1 : 0;
-
-        while (k < expenditure.length - 1) {
-
-            int expRemoved = q.remove();
-            int expInserted = expenditure[k];
-            q.add(expInserted);
-
-            counts[expRemoved]--;
-            counts[expInserted]++;
-
-            k++;
-
-            notificationIssued = checkForFraud(counts, d, maxExpense, expenditure[k]);
-            notifications += notificationIssued ? 1 : 0;
-
-        }
-
-        return notifications;
+    @Test
+    public void testCase0() throws IOException {
+        FraudulentActivityNotification fan = new FraudulentActivityNotification("fraud-testCase0.txt");
+        Assert.assertEquals(1, fan.getResult());
     }
 
 
 
-    private static boolean checkForFraud(int[] count, int windowSize, int maxValue, int expense) {
-        int[] sums = new int[maxValue + 1];
-        sums[0] = count[0];
-        if (windowSize % 2 == 1) {
-            int p = windowSize / 2;
-            for (int i = 1; i < count.length; i++) {
-                int x = sums[i - 1] + count[i];
-                if (x - 1 >= p) {
-                    return expense >= 2 * i;
-                } else {
-                    sums[i] = x;
-                }
-            }
-        } else {
-            int p1 = windowSize / 2 - 1;
-            int p2 = 1 + p1;
-            boolean b1 = false;
-            boolean b2 = false;
-            int x1 = 0;
-            int x2 = 0;
-            for (int i = 1; i < count.length; i++) {
-                int x = sums[i - 1] + count[i];
-                if (x - 1 >= p1 && !b1) {
-                    if (x - 1 >= p2) {
-                        // se sono qui è perché il valore "i" è presente in un intervallo di indici del
-                        // (futuro) array ordinato che include o travalica gli indici che servono a
-                        // calcolare la mediana
-                        return expense >= 2 * i;
-                    } else {
-                        x1 = i;
-                        b1 = true;
-                        sums[i] = x;
-                        continue;
-                    }
-                }
-                if (x - 1 >= p2) {
-                    x2 = i;
-                    b2 = true;
-                }
-                sums[i] = x;
-                if (b1 && b2) {
-                    break;
-                }
-            }
-            return expense >= (x1 + x2);
-        }
-        throw new RuntimeException("non è stato trovato l'indice della mediana");
+    @Test
+    public void testCase1() throws IOException {
+        FraudulentActivityNotification fan = new FraudulentActivityNotification("fraud-testCase1.txt");
+        Assert.assertEquals(633, fan.getResult());
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
 
-        // BufferedWriter bufferedWriter = new BufferedWriter(new
-        // FileWriter(System.getenv("OUTPUT_PATH")));
+    @Test
+    public void testCase2() throws IOException {
+        FraudulentActivityNotification fan = new FraudulentActivityNotification("fraud-testCase2.txt");
+        Assert.assertEquals(770, fan.getResult());
+    }
 
-        String[] nd = scanner.nextLine().split(" ");
 
-        int n = Integer.parseInt(nd[0]);
 
-        int d = Integer.parseInt(nd[1]);
+    @Test
+    public void testCase3() throws IOException {
+        FraudulentActivityNotification fan = new FraudulentActivityNotification("fraud-testCase3.txt");
+        Assert.assertEquals(1026, fan.getResult());
+    }
 
-        int[] expenditure = new int[n];
 
-        String[] expenditureItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < n; i++) {
-            int expenditureItem = Integer.parseInt(expenditureItems[i]);
-            expenditure[i] = expenditureItem;
-        }
+    @Test
+    public void testCase4() throws IOException {
+        FraudulentActivityNotification fan = new FraudulentActivityNotification("fraud-testCase4.txt");
+        Assert.assertEquals(492, fan.getResult());
+    }
 
-        int result = activityNotifications(expenditure, d);
 
-        System.out.println(result);
 
-        // bufferedWriter.write(String.valueOf(result));
-        // bufferedWriter.newLine();
-
-        // bufferedWriter.close();
-
-        scanner.close();
+    @Test
+    public void testCase5() throws IOException {
+        FraudulentActivityNotification fan = new FraudulentActivityNotification("fraud-testCase5.txt");
+        Assert.assertEquals(926, fan.getResult());
     }
 }
