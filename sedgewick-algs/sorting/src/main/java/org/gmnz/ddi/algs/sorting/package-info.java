beb7232a779e5ef16a9c603a1e08b1162e6acd0e
Simone <s3lvatico@gmail.com>
2019-11-06 09:20:48 +0100

    package-info con riassunto sul package di sorting

---
+++ sedgewick-algs/sorting/src/main/java/org/gmnz/ddi/algs/sorting/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Classi che implementano gli algoritmi di ordinamento.
+ * <p>
+ * <h3>Sommario delle classi</h3> {@link ArrayUtils} : metodi per il controllo
+ * dell'ordinamento di un array, conversione in stringa e generazione di array
+ * dal contenuto casuale.
+ * <p>
+ * Classi di implementazione degli algoritmi di ordinamento:
+ * <ul>
+ * <li>Insertion sort
+ * <li>Selection sort
+ * <li>Merge sort
+ * <li>Quick sort
+ * </ul>
+ */
+package org.gmnz.ddi.algs.sorting;
