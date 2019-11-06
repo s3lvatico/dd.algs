6a3d382ca18ad0e7279b64894dce760fb85a279e
Simone <s3lvatico@gmail.com>
2019-11-06 10:32:14 +0100

    aggiunto test per randomDoubleArray

--- sedgewick-algs/sorting/src/test/java/org/gmnz/ddi/algs/sorting/ArrayUtilsTest.java
+++ sedgewick-algs/sorting/src/test/java/org/gmnz/ddi/algs/sorting/ArrayUtilsTest.java
@@ -1,9 +1,11 @@
 package org.gmnz.ddi.algs.sorting;
 
+
 import java.util.Random;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 
 /**
  * ArrayUtilsTest
@@ -24,6 +26,8 @@ public class ArrayUtilsTest {
         v = new int[] { 1, 2, 3 };
         Assert.assertTrue(ArrayUtils.isSorted(v));
     }
+
+
 
     @Test
     public void randomIntArray() {
@@ -48,6 +52,33 @@ public class ArrayUtilsTest {
             for (int x : v) {
                 Assert.assertTrue(x >= lo && x < hi);
             }
+
+        }
+        System.out.println("Sample string representation of an array");
+        int lo = Integer.MIN_VALUE + rnd.nextInt(Integer.MAX_VALUE);
+        int hi = rnd.nextInt(Integer.MAX_VALUE);
+        int length = 1 + rnd.nextInt(Byte.MAX_VALUE);
+        v = ArrayUtils.randomIntArray(length, lo, hi);
+        System.out.println(ArrayUtils.toString(v));
+    }
+
+
+
+    @Test
+    public void randomDoubleArray() {
+        Assert.assertNull(ArrayUtils.randomDoubleArray(-Math.abs(new Random().nextInt()), 0.0d, 0.0d));
+        double[] dblv = ArrayUtils.randomDoubleArray(0, 0.0d, 0.0d);
+        Assert.assertNotNull(dblv);
+        Assert.assertEquals(0, dblv.length);
+
+        Random rnd = new Random();
+        int lowerBound = rnd.nextInt();
+        int range = Math.abs(rnd.nextInt());
+        int length = rnd.nextInt(Byte.MAX_VALUE);
+        dblv = ArrayUtils.randomDoubleArray(length, lowerBound, range);
+        Assert.assertNotNull(dblv);
+        for (double d : dblv) {
+            Assert.assertTrue(d >= lowerBound && d <= lowerBound + range);
         }
     }
 }
