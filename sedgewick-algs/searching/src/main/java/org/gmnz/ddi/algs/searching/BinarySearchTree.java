83c1643ce00c4881640e1016dfa4f8e245e2bbf3
S3lvatico <s3lvatico@gmail.com>
2019-12-03 19:19:01 +0100

    bst : rimossi import inutili

--- sedgewick-algs/searching/src/main/java/org/gmnz/ddi/algs/searching/BinarySearchTree.java
+++ sedgewick-algs/searching/src/main/java/org/gmnz/ddi/algs/searching/BinarySearchTree.java
@@ -1,8 +1,6 @@
 package org.gmnz.ddi.algs.searching;
 
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
