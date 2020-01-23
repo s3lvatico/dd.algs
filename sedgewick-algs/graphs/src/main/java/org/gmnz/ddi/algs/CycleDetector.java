462faec71a0465ba00bd78b48e360b326eb7fb20
Simone <s3lvatico@gmail.com>
2020-01-23 18:00:53 +0100

    aggiunto CycleDetector e test (test da tracciare)

---
+++ sedgewick-algs/graphs/src/main/java/org/gmnz/ddi/algs/CycleDetector.java
@@ -0,0 +1,28 @@
+package org.gmnz.ddi.algs;
+
+public class CycleDetector {
+
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public CycleDetector(Graph g) {
+        marked = new boolean[g.V()];
+        for (int s = 0; s < g.V(); s++) {
+            if (!marked[s]) 
+            dfs(g, s, s);
+        }
+    }
+
+    private void dfs(Graph g, int v, int u) {
+        marked[v] = true;
+        for (int w : g.adj(v))
+            if (!marked[w])
+                dfs(g, w, v);
+            else
+                if (w != u)
+                    hasCycle = true;
+    }
+
+    public boolean hasCycle() { return hasCycle; }
+
+}
---
+++ sedgewick-algs/graphs/src/test/java/org/gmnz/ddi/algs/TestCycleDetector.java
@@ -0,0 +1,40 @@
+package org.gmnz.ddi.algs;
+
+
+import static org.junit.Assert.fail;
+
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestCycleDetector {
+
+    private Graph g;
+
+    @Before
+    public void beforeEachTest() {
+
+        try (Scanner s = new Scanner(GraphCreationTest.class.getResourceAsStream("/tinyGraph.txt"), "UTF-8")) {
+            g = GraphFactory.createGraph(s);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+
+        GraphUtils.printInfo(g);
+    }
+
+
+
+    @Test
+    public void cycleDetectorTest() {
+        CycleDetector cd = new CycleDetector(g);
+        Assert.assertTrue(cd.hasCycle());
+        System.out.format("Provided graph has at least a cycle");
+    }
+
+}
