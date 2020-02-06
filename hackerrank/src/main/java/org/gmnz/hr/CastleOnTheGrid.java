07eec40e1770fa9b6600d167b8e8a71a1fff4a26
Simone <s3lvatico@gmail.com>
2020-02-06 12:00:06 +0100

    metodo per le adiacenze in un grafo linearizzato

--- hackerrank/src/main/java/org/gmnz/hr/CastleOnTheGrid.java
+++ hackerrank/src/main/java/org/gmnz/hr/CastleOnTheGrid.java
@@ -4,17 +4,73 @@ package org.gmnz.hr;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class CastleOnTheGrid {
 
+
    static int minimumMoves(String[] grid, int startX, int startY, int goalX, int goalY) {
+
+      StringBuilder sbGrid = new StringBuilder();
+      for (String line : grid)
+         sbGrid.append(line);
+
+      String linearGrid = sbGrid.toString();
+
+      boolean[] marked = new boolean[linearGrid.length()];
+      Arrays.fill(marked, false);
+
+      int[] edgeTo = new int[linearGrid.length()];
+
+      int startCell = cell(grid.length, startX, startY);
+      int goalCell = cell(grid.length, goalX, goalY);
+
+      ArrayDeque<Integer> q = new ArrayDeque<>();
+
+      // tutto pronto, ora BFS
+
 
       return -1;
    }
 
+
+
+   private static int cell(int l, int x, int y) {
+      return x + l * y;
+   }
+
+
+
+   private static int[] adjacencies(String lg, int n, int id) {
+      ArrayList<Integer> adj = new ArrayList<>();
+
+      // cella a nord
+      if (id >= n && lg.charAt(id - n) != 'X') {
+         adj.add(id - n);
+      }
+      // cella a est
+      if (id < lg.length() - 1 && lg.charAt(id + 1) != 'X') {
+         adj.add(id + 1);
+      }
+      // cella a sud
+      if (id < lg.length() - n && lg.charAt(id + n) != 'X') {
+         adj.add(id + n);
+      }
+      // cella a ovest
+      if (id > 0 && lg.charAt(id - 1) != 'X') {
+         adj.add(id - 1);
+      }
+
+      return adj.stream().mapToInt(x -> x.intValue()).toArray();
+   }
+
+
    private static final Scanner scanner = new Scanner(System.in);
+
 
    public static void main(String[] args) throws IOException {
       BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
@@ -49,14 +105,3 @@ public class CastleOnTheGrid {
       scanner.close();
    }
 }
-
-/*
- * 
- * "type": "java", "name": "CodeLens (Launch) - CastleOnTheGrid", "request":
- * "launch", "mainClass": "org.gmnz.hr.CastleOnTheGrid", "projectName":
- * "hackerrank", "env": { "OUTPUT_PATH":
- * "C:\\dev\\src\\dd.algs\\hackerrank\\resources\\q.txt" }, "args":
- * "< C:\\dev\\src\\dd.algs\\hackerrank\\target\\classes\\castleOnTheGrid-in01.txt"
- * },
- * 
- */
